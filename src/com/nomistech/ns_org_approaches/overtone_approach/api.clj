7276afa2d792797f02b9c24956a8e6d1cc17029c
Simon Katz <git@nomistech.com>
2013-05-28 18:04:17 +0100

    Jiggling about.

--- src/com/nomistech/ns_org_approaches/overtone_approach/api.clj
+++ src/com/nomistech/ns_org_approaches/overtone_approach/api.clj
@@ -2,9 +2,13 @@
   (:require [com.nomistech.clojure-utils :as u]
             [com.nomistech.ns-org-approaches.overtone-approach.impl-1]))
 
+(u/import-vars com.nomistech.ns-org-approaches.overtone-approach.protocols
+               [f1])
+
 (u/import-vars com.nomistech.ns-org-approaches.overtone-approach.impl-1 ; TODO: Add capability for using namespace aliases
                [x-val
-                x-fun])
+                x-fun
+                create-r])
 
 
 ;;; We can import something even though we don't have a dependency
@@ -12,9 +16,3 @@
 ;;; on `ns` forms to fail to work. That includes clojure.tools.namespace
 ;;; and Midje.
 (u/import-vars com.nomistech.clojure-dev-utils [nomis-pp-classpath])
-
-(u/import-vars com.nomistech.ns-org-approaches.overtone-approach.protocols
-               [f1])
-
-(u/import-vars com.nomistech.ns-org-approaches.overtone-approach.impl-1
-               [create-r])
