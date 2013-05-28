79a2dff6e5c0d87ec6fd8113c123dcda7c25bd18
Simon Katz <git@nomistech.com>
2013-05-28 18:48:21 +0100

    `import-vars` now handles namespace aliases, so use aliases.

--- src/com/nomistech/ns_org_approaches/overtone_approach/api.clj
+++ src/com/nomistech/ns_org_approaches/overtone_approach/api.clj
@@ -1,18 +1,19 @@
 (ns com.nomistech.ns-org-approaches.overtone-approach.api
   (:require [com.nomistech.clojure-utils :as u]
-            [com.nomistech.ns-org-approaches.overtone-approach.impl-1]))
+            [com.nomistech.ns-org-approaches.overtone-approach.protocols
+             :as protocols]
+            [com.nomistech.ns-org-approaches.overtone-approach.impl-1
+             :as impl-1]))
 
-(u/import-vars com.nomistech.ns-org-approaches.overtone-approach.protocols
-               [f1])
+(u/import-vars protocols [f1])
 
-(u/import-vars com.nomistech.ns-org-approaches.overtone-approach.impl-1 ; TODO: Add capability for using namespace aliases
-               [x-val
-                x-fun
-                create-r])
+(u/import-vars impl-1 [x-val
+                       x-fun
+                       create-r])
 
-
-;;; We can import something even though we don't have a dependency
-;;; on its namespace in our `ns` form. This will cause tools that rely
-;;; on `ns` forms to fail to work. That includes clojure.tools.namespace
-;;; and Midje.
-(u/import-vars com.nomistech.clojure-dev-utils [nomis-pp-classpath])
+(do
+  ;; We can import something even though we don't have a dependency
+  ;; on its namespace in our `ns` form. This will cause tools that rely
+  ;; on `ns` forms to fail to work. That includes clojure.tools.namespace
+  ;; and Midje.
+  (u/import-vars com.nomistech.clojure-dev-utils [nomis-pp-classpath]))
